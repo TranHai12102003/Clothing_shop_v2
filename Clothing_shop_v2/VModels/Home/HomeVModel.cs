55a2db0d2f7fa81fe9271abd13cf769a307a081e
TranHai12102003 <trantrunghai141@gmail.com>
2025-05-05 20:38:24 +0700

    load dữ liệu trang chủ

--- Clothing_shop_v2/VModels/Home/HomeVModel.cs
+++ Clothing_shop_v2/VModels/Home/HomeVModel.cs
@@ -5,7 +5,7 @@ namespace Clothing_shop_v2.VModels.Home
     public class HomeVModel
     {
         public List<ProductGetVModel> Products { get; set; }
-        //public List<BannerGetVModel> Banners { get; set; } = new List<BannerGetVModel>();
+        public List<BannerGetVModel> Banners { get; set; } = new List<BannerGetVModel>();
         //public List<PromotionGetVmodel> Promotions { get; set; } = new List<PromotionGetVmodel>();
         public List<CategoryGetVModel> Categories { get; set; }
 
